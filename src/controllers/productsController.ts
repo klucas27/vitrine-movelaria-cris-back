dd088d1e5ebee2b895927cbbc096840a392e2c02
Kresley <kresleylucas.r@gmail.com>
2025-12-17 22:00:08 -0300

    feat: add usuario model and implement CRUD operations for products

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,4 +1,3 @@
-
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
@@ -40,5 +39,60 @@ export const getAllProducts = async (req: Request, res: Response) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Erro ao buscar itens" });
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const deleted = await prisma.vititens.delete({
+      where: { vitId: Number(id) }
+    });
+    res.json({ message: "Produto removido com sucesso", deleted });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erro ao remover produto" });
+  }
+};
+
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { nome, descricao, categoria, material, preco, imagem_url } = req.body;
+    const novoProduto = await prisma.vititens.create({
+      data: {
+        vitNome: nome,
+        vitDescricao: descricao,
+        vitCategoria: categoria,
+        vitMaterial: material,
+        vitPreco: preco,
+        vitImagem: imagem_url,
+      },
+    });
+    res.status(201).json(novoProduto);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erro ao cadastrar produto" });
+  }
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { nome, descricao, categoria, material, preco, imagem_url } = req.body;
+  try {
+    const produtoAtualizado = await prisma.vititens.update({
+      where: { vitId: Number(id) },
+      data: {
+        vitNome: nome,
+        vitDescricao: descricao,
+        vitCategoria: categoria,
+        vitMaterial: material,
+        vitPreco: preco,
+        vitImagem: imagem_url,
+      },
+    });
+    res.json(produtoAtualizado);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erro ao atualizar produto" });
   }
 };
--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,13 +1,12 @@
-
 import { Router } from "express";
-import { getActiveProducts, getAllProducts } from "../controllers/productsController";
+import { getActiveProducts, getAllProducts, deleteProduct, createProduct, updateProduct } from "../controllers/productsController";
 
 const router = Router();
 
-
 router.get("/all", getAllProducts);
 router.get("/ativos", getActiveProducts);
-
-// router.post("/createmovel", createMovel);
+router.delete('/:id', deleteProduct);
+router.post("/createProduct", createProduct);
+router.put("/update/:id", updateProduct);
 
 export default router;
